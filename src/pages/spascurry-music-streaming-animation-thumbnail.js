07a7f29650f80ddcd7064880b9b954f9cabb3f37
kmasanori <kmasanori0221@gmail.com>
2022-01-02 15:14:41 +0900

    feat: add control panel on youtube iframe.

--- src/pages/spascurry-music-streaming-animation-thumbnail.js
+++ src/pages/spascurry-music-streaming-animation-thumbnail.js
@@ -4,7 +4,16 @@ import WorkDetail from "../templates/workDetail"
 export default function SpasCurryMusicStreamingAnimationThumbnail({ location }) {
   const workElement = (
     <div className="w-full" style={{ aspectRatio: "16 / 9" }}>
-      <iframe className="w-full h-full" width="640" height="360" src="https://www.youtube.com/embed/rL4M3cXNKRY?controls=0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <iframe
+        className="w-full h-full"
+        width={640}
+        height={360}
+        src="https://www.youtube.com/embed/rL4M3cXNKRY"
+        title="YouTube video player"
+        frameBorder={0}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
     </div>
   )
 
