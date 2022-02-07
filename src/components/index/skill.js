ff994bf4a898e9838436888d66f5ee369715cc3c
kmasanori <kmasanori0221@gmail.com>
2022-02-07 17:20:49 +0900

    fix: add new skills.

--- src/components/index/skill.js
+++ src/components/index/skill.js
@@ -6,7 +6,7 @@ export default function Skill() {
       <div className="w-10/12 max-w-md mx-auto">
         <h1 className="mb-4">Skills</h1>
 
-        <p className="text-xs leading-relaxed text-gray-400">WEBシステム開発 / WEBサイト制作 / WEBデザイン / イラスト / アニメーション / 動画編集</p>
+        <p className="text-xs leading-relaxed text-gray-400">WEBシステム開発 / WEBサイト制作 / WEBデザイン / イラスト / アニメーション / 動画制作 / バナー制作 / 広告制作</p>
       </div>
     </section>
   )
